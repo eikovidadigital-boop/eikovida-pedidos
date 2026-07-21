0f09f6b9ab5328bc6894c959d0b1027eaa6f064f
eikovidadigital-boop <eikovidadigital@gmail.com>
2026-07-21 13:21:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // EikoVida Pedidos - service worker
-const CACHE = "eiko-pedidos-v6";
-const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo.png", "./simbolo.png"];
+const CACHE = "eiko-pedidos-v7";
+const SHELL = ["./", "./index.html", "./instalar.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo.png", "./simbolo.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
